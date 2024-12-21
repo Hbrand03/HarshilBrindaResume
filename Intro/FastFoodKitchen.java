3f852f8cc927a07b6c5a9c3b9ce73098bcb3a39f
Hbrand03 <hbrinda@uncc.edu>
2024-12-20 19:47:52 -0500

    Add files via upload

--- Intro/FastFoodKitchen.java
+++ Intro/FastFoodKitchen.java
@@ -1,115 +1,244 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package main;
+package fastfoodkitchen;
+
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 /**
  *
- * @author harsh
+ * ITSC 1213 
+ * University of North Carolina at Charlotte
  */
+
 public class FastFoodKitchen {
-    
-    private ArrayList<Order> orderList = new ArrayList<>();
+
+    private ArrayList<BurgerOrder> orderList = new ArrayList();
+
     private static int nextOrderNum = 1;
-    /**
-     * none since constructor that initializes
-     */
-    public FastFoodKitchen()
-    {
-        Order order1 = new Order(3,15,4,10,false,nextOrderNum);
-        incrementNextOrderNum();
-        orderList.add(order1);
-        
-        Order order2 = new Order(0,0,3,3,false,2);
-        incrementNextOrderNum();
-        orderList.add(order2);
-        
-        Order order3 = new Order(1,1,0,2,false,3);
-        incrementNextOrderNum();
-        orderList.add(order3);
+   
+    FastFoodKitchen() {
+        loadOrdersFromFile("burgerOrders.csv");
     }
-    
-    /**
-     *
-     * @return next order number
-     */
-    public int getNextOrderNum()
-    {
+
+    public static int getNextOrderNum() {
         return nextOrderNum;
     }
-    
-    private static void incrementNextOrderNum()
-    {
+
+    private void incrementNextOrderNum() {
         nextOrderNum++;
     }
-    
-    /**
-     *
-     * @param ham
-     * @param cheese
-     * @param veggie
-     * @param soda
-     * @param toGo
-     * @return next order number
-     */
-    public int addOrder(int ham, int cheese, int veggie, int soda, boolean toGo)
-    {
-        Order order = new Order(ham, cheese, veggie, soda, toGo,nextOrderNum);
-        orderList.add(order);
+
+    public int addOrder(int ham, int cheese, int veggie, int soda, boolean toGo) {
+        int orderNum = getNextOrderNum();
+        orderList.add(new BurgerOrder(ham, cheese, veggie, soda, toGo, orderNum));
         incrementNextOrderNum();
-              
-        System.out.println("Order number: ");
-        return getNextOrderNum();
+        orderCallOut(orderList.get(orderList.size() - 1));
+        return orderNum;
+
     }
-    
-    /**
-     *
-     * @return if the last order can be cancelled or not
-     */
-    public boolean cancelLastOrder()
-    {
-        if(nextOrderNum > 0)
-        {
-            nextOrderNum--;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    
-    /**
-     *
-     * @return how many orders left
-     */
-    public int getNumOrdersPending()
-    {
-        return orderList.size();
-    }
-    
-    public boolean isOrderDone(int orderID){
-         for (Order order : orderList) {
-            if (order.getOrderNum()== orderID) {
-                // Order is still in progress
+
+    public boolean isOrderDone(int orderID) {
+        for (int i = 0; i < orderList.size(); i++) {
+            if (orderList.get(i).getOrderNum() == orderID) {
                 return false;
             }
         }
         return true;
     }
-    
+
     public boolean cancelOrder(int orderID) {
-         for (Order order : orderList) {
-            if (order.getOrderNum()== orderID) {
-                orderList.remove(order);
+        for (int i = 0; i < orderList.size(); i++) {
+            if (orderList.get(i).getOrderNum() == orderID) {
+                orderList.remove(i);
                 return true;
             }
         }
         return false;
     }
 
+    public int getNumOrdersPending() {
+        return orderList.size();
+    }
+
+    public boolean cancelLastOrder() {
+
+        if (!orderList.isEmpty()) { // same as  if (orderList.size() > 0) 
+            orderList.remove(orderList.size() - 1);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void orderCallOut(BurgerOrder order) {
+        if (order.getNumCheeseburgers() > 0) {
+            System.out.println("You have " + order.getNumHamburger() + " hamburgers");
+        }
+        if (order.getNumCheeseburgers() > 0) {
+            System.out.println("You have " + order.getNumCheeseburgers() + " cheeseburgers");
+        }
+        if (order.getNumVeggieburgers() > 0) {
+            System.out.println("You have " + order.getNumVeggieburgers() + " veggieburgers");
+        }
+        if (order.getNumSodas() > 0) {
+            System.out.println("You have " + order.getNumSodas() + " sodas");
+        }
+
+    }
+
+    public void completeSpecificOrder(int orderID) {
+        for (int i = 0; i < orderList.size(); i++) {
+            if (orderList.get(i).getOrderNum() == orderID) {
+                System.out.println("Order number " + orderID + " is done!");
+                if (orderList.get(i).isOrderToGo()) {
+                    orderCallOut(orderList.get(i));
+                }
+                orderList.remove(i);
+            }
+        }
+
+    }
+
+    public void completeNextOrder() {
+        int nextOrder = orderList.get(0).getOrderNum();
+        completeSpecificOrder(nextOrder);
+
+    }
+
+    // Part 2
+    public ArrayList<BurgerOrder> getOrderList() {
+        return orderList;
+    }
+
+    public int findOrderSeq(int whatWeAreLookingFor) {
+        for (int j = 0; j < orderList.size(); j++) {
+            if (orderList.get(j).getOrderNum() == whatWeAreLookingFor) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+//    public int findOrderBin(int whatWeAreLookingFor) {
+//        int left = 0;
+//        int right = orderList.size() - 1;
+//        while (left <= right) {
+//            int middle = (left + right) / 2;
+//            if (whatWeAreLookingFor < orderList.get(middle).getOrderNum()) {
+//                right = middle - 1;
+//            } else if (whatWeAreLookingFor > orderList.get(middle).getOrderNum()) {
+//                left = middle + 1;
+//            } else {
+//                return middle;
+//            }
+//        }
+//        return -1;
+//    }
+
+  public int findOrderBin(int orderID){
+        int left = 0;
+        int right = orderList.size()-1;
+        while (left <= right){
+            int middle = ((left + right)/2);
+            if (orderID < orderList.get(middle).getOrderNum()){
+                right = middle-1;
+            }
+            else if(orderID > orderList.get(middle).getOrderNum()){
+                left = middle +1;
+            }
+            else{
+                return middle;
+            }
+        }
+        return -1;
+        
+    }
+    public void selectionSort(){
+        for (int i = 0; i< orderList.size()-1; i++){
+            int minIndex = i;
+            for (int k = i+1; k < orderList.size(); k++){
+                if (orderList.get(minIndex).getTotalBurgers() > orderList.get(k).getTotalBurgers()){
+                    minIndex = k;
+                }
+            }
+            BurgerOrder temp = orderList.get(i);
+            orderList.set(i , orderList.get(minIndex));
+            orderList.set(minIndex, temp);
+        }
+    }
+
+    public void insertionSort() {
+        for (int j = 1; j < orderList.size(); j++) {
+            BurgerOrder temp = orderList.get(j);
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp.getTotalBurgers() < orderList.get(possibleIndex - 1).getTotalBurgers()) {
+                orderList.set(possibleIndex, orderList.get(possibleIndex - 1));
+                possibleIndex--;
+            }
+            orderList.set(possibleIndex, temp);
+        }
+    }
     
+//    public void selectionSort() { //weird method!
+//
+//        for (int j = 0; j < orderList.size() - 1; j++) {
+//            int minIndex = j;
+//            for (int k = j + 1; k < orderList.size(); k++) {
+//
+//                 if (orderList.get(minIndex).getTotalBurgers() > orderList.get(j).getTotalBurgers()){
+//                    minIndex = k;
+//                }
+//            }
+//            BurgerOrder temp = orderList.get(j);
+//            orderList.set(j, orderList.get(minIndex));
+//            orderList.set(minIndex, temp);
+//
+//        }
+//    }
     
+    public void loadOrdersFromFile(String filename){
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            br.readLine();
+            while((line = br.readLine()) != null){
+                String[] values = line.split(",");
+                int ham = Integer.parseInt(values[0]);
+                int cheese = Integer.parseInt(values[1]);
+                int veggie = Integer.parseInt(values[2]);
+                int sodas = Integer.parseInt(values[3]);
+                boolean toGo = Boolean.parseBoolean(values[4]);
+                
+                BurgerOrder order = new BurgerOrder(ham, cheese, veggie, sodas, toGo, getNextOrderNum());
+                orderList.add(order);
+                incrementNextOrderNum();
+            }
+        } catch (IOException | NumberFormatException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void updateOrdersFile(String filename){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            for (BurgerOrder order : orderList) {
+                bw.write(order.getNumHamburger() + "," +
+                         order.getNumCheeseburgers() + "," +
+                         order.getNumVeggieburgers() + "," +
+                         order.getNumSodas() + "," +
+                         order.isOrderToGo());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void endOfDay(String updatedOrdersFile) {
+        // Process end-of-day tasks
+        updateOrdersFile(updatedOrdersFile);
+    }
+
 }
